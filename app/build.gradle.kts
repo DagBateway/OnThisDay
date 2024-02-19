b8264fdc60cd69da5064d6ab838dd300a4e3688e
Alberto <alberto.camillo@outlook.com>
2024-02-19 18:09:20 +1100

    Minor network updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.albertocamillo.onthisday"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 5
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
