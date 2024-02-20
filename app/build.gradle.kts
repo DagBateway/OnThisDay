8e1dd28bf6c00489ea27db592c9ec90f96f42b5a
Alberto <alberto.camillo@outlook.com>
2024-02-20 17:55:13 +1100

    Updated version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.albertocamillo.onthisday"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "2.2"
+        versionCode = 7
+        versionName = "3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
